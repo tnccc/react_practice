464d5a6900133a5499914d0c96c24ee105ed30ab
tnccc <irei.frontend@gmail.com>
2023-05-22 22:32:56 +0900

    useStateの練習問題を解く

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import StateArray from './components/StateArray';
+import SampleState from './components/SampleState';
+
 
 function App() {
   return (
     <div className="App">
-      <StateArray />
+      <SampleState />
     </div>
   );
 }
